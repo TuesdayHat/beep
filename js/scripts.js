0cdc861ded027794c065239e2d9540b21d240cc0
Adam Calhoun <a.t.calhoun@gmail.com>
2017-12-08 09:21:25 -0800

    pass 1st spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
+function beepCheck(num){
+  var check = num;
+  if (check === 1){
+    return "Beep!"
+  }
+
+  return check
+}
+
+
+//FRONTEND BELOW THIS LINE ===--------------------------------
+
 $(document).ready(function() {
   $("#FORM").submit(function(event) {
     event.preventDefault();
     var input = parseInt($("#INPUT").val());
-    var result = input;
+    var result = beepCheck(input);
 
     $("#result").text(result);
 
